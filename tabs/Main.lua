697cbab57ef0d7135630b965358e0a48b3f777cb
Jaewon-Seo at codea-scm <Jaewon-Seo@codea-scm.aws.mapote.com>
2016-11-07 16:08:38 +0000

    Initial

---
+++ tabs/Main.lua
@@ -0,0 +1,26 @@
+-- Lesson 7
+--By : Jaewon seo
+-- Created October 2016
+-- Created for ICS20
+
+-- coment
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing here
+    
+    sprite("Project:Blue Back Circle Button", WIDTH/2, HEIGHT/2, 500, 500)
+    
+end
+
